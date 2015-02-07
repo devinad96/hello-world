407a276e0b20c77fb333ceb0dadce9eb77014d2c
Devina Darmawan <darmawan.devina@gmail.com>
2015-02-06 19:55:23 -0800

    The original commit for the password project

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World\n";
+   cout << "My family name is Darmawan" << endl;
+   cout << "My foothill ID number is 20212011" << endl << endl;
+   cout << "Syllabus Details :" << endl;
+   cout << "1. When we're posting an assignment question, we have to modify "
+      << "our code first so that it doesn't show our original answer." << endl;
+   cout << "2. The grades are 72% from lab assignments, 4% from public "
+      << "discussions, and 24% from exams." << endl << endl;
+   cout << "Specific Lab Details :" << endl;
+   cout << "1. We have to make sure that our program is working correctly "
+      << "because it's worth zero point if it doesn't work." << endl;
+   cout << "2. In our programs, the run have to fully match the source. "
+      << "There will be a large penalty for those who edit their run." << endl;
+   cout << "3. Don't be afraid to ask questions because we might also help "
+      << "other friends who get confused on the same problem." << endl;
+   return 0;
+}
+
+/* ----------------------------- paste of run -------------------------------
+
+Hello World
+My family name is Darmawan
+My foothill ID number is 20212011
+
+Syllabus Details :
+1. When we're posting an assignment question, we have to modify our code first so that it doesn't show our original answer.
+2. The grades are 72% from lab assignments, 4% from public discussions, and 24% from exams.
+
+Specific Lab Details :
+1. We have to make sure that our program is working correctly because it's worth zero point if it doesn't work.
+2. In our programs, the run have to fully match the source. There will be a large penalty for those who edit their run.
+3. Don't be afraid to ask questions because we might also help other friends who get confused on the same problem.
+
+--------------------------------------------------------------------------- */
